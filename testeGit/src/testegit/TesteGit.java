4ba3a8fd3f7fb627af99eff8b0958ceed84f81ab
Sidney Junior <sidney_souza-junior@estudante.senai.sc.br>
2020-05-09 10:19:14 -0300

    Mensagem de Boa noite

--- testeGit/src/testegit/TesteGit.java
+++ testeGit/src/testegit/TesteGit.java
@@ -8,6 +8,7 @@ public class TesteGit {
     public static void main(String[] args) {
         System.out.println("Bom dia");
         System.out.println("Boa tarde");
+        System.out.println("Boa noite");
     }
     
 }
